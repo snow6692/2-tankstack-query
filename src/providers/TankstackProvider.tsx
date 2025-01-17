d1b8c9c1738504cdc227f809bf098b0192a53c36
snow6692 <ahmedha258258@gmail.com>
2025-01-17 21:11:38 +0200

    TankstackQueryProvider Added

---
+++ src/providers/TankstackProvider.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { ReactNode, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+function TankstackProvider({ children }: { children: ReactNode }) {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default TankstackProvider;
